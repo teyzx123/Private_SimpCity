8335db59450f267fb4d9508d70be168e0e3c4657
ChristopherTeyWeixian <64050082+ChristopherTeyWeixian@users.noreply.github.com>
2022-01-08 17:23:05 +0800

    Create test_incremental_Two_Chris.py

---
+++ tests/test_incremental_Two_Chris.py
@@ -0,0 +1,18 @@
+from files.Game_Options import *
+import pytest
+
+     
+
+def test_save_game(): #
+    board=Board()
+    newboard=board.New_Board()
+    assert board.Save_Board() == True
+def test_load_game(): #
+    board=Board()
+    assert board.Load_Board()==True
+@pytest.mark.parametrize("BuildingType,expectedResult",[("BCH",True),("FAC",True),("HSE",True),("SHP",True),("HWY",True)])
+def test_play_game_after_load(BuildingType,expectedResult): #
+    board=Board()
+    board.Load_Board()
+    result=building_choice(board,"B1",BuildingType)
+    assert result== True
